d21a690517e0ad303a81e3f7c95b2c6d3d3cfd2d
Chris Fredricksen <cmfredricksen@gmail.com>
2022-07-17 19:40:03 -0500

    added content to index

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -6,6 +6,10 @@
 	/>
 </svelte:head>
 
+Welcome to my Road trip! I came up with the idea for this site while looking at different development roadmaps. I hadn't looked at one in quite a while and wanted to see if I had made any progress in regards to the map. I have! Then I wanted to look at other ones and that made me think I should share my roadmap. It is not done yet as I am still learning development. So this is my road trip, which I am still on. Hope this can inspire you or give you insight. 
+
+Importantly, don't give up!
+
 
 <style>
 </style>
